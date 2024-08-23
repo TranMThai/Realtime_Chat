782e9c14e12ba35d7ac4b9c5bd1f736fef718caf
Trần Minh Thái <thaitranminh564@gmail.com>
2024-08-23 17:15:56 +0700

    basic socket

---
+++ RealtimeChat_Server/src/main/java/com/server/realtime_chat/controller/ChatController.java
@@ -0,0 +1,18 @@
+package com.server.realtime_chat.controller;
+
+import com.server.realtime_chat.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+}
---
+++ RealtimeChat_Server/src/main/java/com/server/realtime_chat/model/MessageType.java
@@ -0,0 +1,7 @@
+package com.server.realtime_chat.model;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
